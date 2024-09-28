ec39e860102ec15317c5e1c916cda52665c6f67d
kal-kidan <kal05627356@gmail.com>
2024-09-28 03:10:28 -0700

    cors enabled

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
